f234ed914506c240c84ce269ba3b412cdc135251
NitzanShahar <68427888+NitzanShahar@users.noreply.github.com>
2024-07-19 10:48:13 +0300

    individual sim model-based

---
+++ Lesson_3_Model-based_learning_in_a_two-step_task/R/generate_two_subjects.R
@@ -0,0 +1,125 @@
+library(dplyr)
+rndwlk = read.csv('R/functions/rndwlk.csv',header=F)
+
+cfg = list(
+  Nblocks          = 4,
+  Ntrials          = 200,
+  Nstates          = 2,
+  Narms            = 2, #number of arms in the task
+  Nraffle          = 2, #number of arms offered for selection each trial
+  rndwlk           = t(rndwlk)
+)
+parameters=c(0.3,0.7,3,1) #change the fourth parameter
+
+#### simulate Rescorla-Wagner block for participant ----
+sim.block = function(parameters,cfg){ 
+  
+  #pre-allocation
+  
+  #set parameters
+  alpha = parameters[1]
+  lambda = parameters[2]
+  beta  = parameters[3]
+  omega  = parameters[4]
+  
+  
+  #set initial var
+  Nstates            = cfg$Nstates #2 states in second stage
+  Narms              = cfg$Narms #2 offered options in each state
+  Nblocks            = cfg$Nblocks 
+  Ntrials   = cfg$Ntrials
+  expvalues          = t(cfg$rndwlk)
+  state_transition   = matrix(c(0.7,0.3,0.3,0.7),ncol=2)
+  df                 = data.frame()
+  
+  for (block in 1:Nblocks){
+    
+    #initialize Qmf values on each block
+    Qmf1               = rep(0.5,Narms) #stage 1 - only one state
+    Qmf2               = matrix(0.5,Nstates,Narms) #stage 2 - two states
+    
+    #initialize Qmb values - only for first stage - second stage is identical to MF
+    Qmb1               = rep(0.5,Narms)
+    
+    for (trial in 1:Ntrials){
+      
+      #define first stage Qmb values based on MF values of second stage and state transitions.
+      Qmb1[1] = state_transition[1,1]*max(Qmf2[1,])+state_transition[2,1]*max(Qmf2[2,])
+      Qmb1[2] = state_transition[1,2]*max(Qmf2[1,])+state_transition[2,2]*max(Qmf2[2,])
+      
+      #integrate MF and MB values modulated by omega parameter
+      Qnet1     = omega*Qmb1+(1-omega)*Qmf1
+      
+      #make first-stage choice
+      p1        = exp(beta*Qnet1) / sum(exp(beta*Qnet1))
+      choice1   = sample(1:Narms,1,prob=p1)
+      unchosen1 = c(1:Narms)[-choice1]
+      
+      #sample state transition based on first stage choice
+      state=sample(1:Nstates,1,prob=state_transition[,choice1])
+      state_prob = state_transition[state,choice1]
+      
+      #second stage choice
+      p2        = exp(beta*Qmf2[state,]) / sum(exp(beta*Qmf2[state,]))
+      choice2   = sample(1:Narms,1,prob=p2)
+      unchosen2 = c(1:Narms)[-choice2]
+      
+      #sample outcome
+      if(state==1){
+        expval_ch = expvalues[choice2,trial]
+        expval_unch = expvalues[unchosen2,trial]
+      }
+      else{
+        expval_ch = expvalues[choice2+2,trial] #for state B, it is 3rd and 4th row of expvalues csv 
+        expval_unch = expvalues[unchosen2+2,trial]
+        
+      }
+      reward = sample(0:1,1,prob=c(1-expval_ch,expval_ch))
+      
+      #save trial's data
+      
+      #create data for current trials
+      dfnew=data.frame(
+        block                = block,
+        trial                = trial,
+        first_trial_in_block = (trial==1)*1,
+        choice1              = choice1,
+        state                = state,
+        state_prob           = state_prob,
+        choice2              = choice2,
+        expval_ch            = expval_ch,
+        expval_unch          = expval_unch,
+        reward               = reward
+      )
+      
+      
+      #bind to the overall df
+      df=rbind(df,dfnew)
+      
+      #updating Qmf values
+      
+      #Prediction erros
+      PE2 = reward - Qmf2[state,choice2] #second stage reward-based prediction error
+      PE1 = Qmf2[state,choice2]-Qmf1[choice1] #first stage prediction error
+      
+      #second stage update
+      Qmf2[state,choice2]=Qmf2[state,choice2]+alpha*PE2
+      #first stage update with an eligibility trace for the second-stage prediction error
+      Qmf1[choice1] = Qmf1[choice1] + alpha*PE1+lambda*alpha*PE2
+      
+    }
+  }     
+  return (df)
+}
+df=sim.block(parameters,cfg)
+
+df=df%>%
+  mutate(reward=factor(reward),
+         reward_oneback=lag(reward),
+         repeat_state = state==lag(state),
+         stay1=choice1==lag(choice1),
+         stay2=choice2==lag(choice2),
+         transition=if_else(state_prob==0.7,"common","rare"),
+         previous_transition=lag(transition))%>%na.omit()
+
+df%>%group_by(reward_oneback,previous_transition)%>%summarise(mean(stay1))
